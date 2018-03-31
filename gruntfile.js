8c9ac9fc60319a287c4a5be6be016e2e6d0c9a05
Mateusz <mateusz.gibki@gmail.com>
2018-03-31 20:55:28 +0200

    31.03. changes

--- gruntfile.js
+++ gruntfile.js
@@ -3,18 +3,33 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     sass: {
-          options: {
-            sourceMap: true
-          },
-          dist: {
-            files: {
-              'css/style.css': 'sass/style.sass'
-            }
-          }
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/style.css': 'sass/style.sass'
         }
+      }
+    },
+    autoprefixer: {
+      dist: {
+        files: {
+          'css/style.css': 'css/style.css'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.sass',
+        tasks: ['sass']
+      }
+    }
   });
   // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
   // Default task(s).
-  grunt.registerTask('default', ["sass"]);
+  grunt.registerTask('default', ["sass", "autoprefixer", "watch"]);
 };
